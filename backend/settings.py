43a7b85c10b6b869ebd6091a7567e12094f79c30
dtorot <dtorot@opensai.org>
2021-08-09 15:47:06 -0500

    settings to heroku

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,6 +88,7 @@ DATABASES = {
     }
 }
 # Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
